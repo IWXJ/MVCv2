0e00040adc97fd035d5f812533fa4508f913a718
Ole <obsoerensen@users.noreply.github.com>
2015-03-26 23:00:16 +0100

    Nye data-klasser

---
+++ src/java/Entity/Pizza.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import Entity.Topping;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author OBS
+ */
+@Entity
+public class Pizza implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String desc;
+    private Double price;
+    private ArrayList<Topping> toppingList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+         this.desc = desc;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Topping)) {
+            return false;
+        }
+        Pizza other = (Pizza) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return ( (other.name == this.name) 
+                && (other.desc == this.desc) 
+                && (other.price == this.price) ) ;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.Pizza[ id=" + id + " ]";
+    }
+    
+}
